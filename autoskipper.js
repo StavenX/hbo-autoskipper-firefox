568c03570c324ea57f03a9115fb03c56707f6c1a
Staven <edvin_myhren@hotmail.com>
2023-03-09 23:03:39 +0100

    Allows to break out of interval, if extension is disabled while watching an episode

--- autoskipper.js
+++ autoskipper.js
@@ -7,11 +7,11 @@ SkipCheckSeconds = 0.5;
 /* The several tags that identifies a skippable button */
 SkipButtonTags = ["SkipButton", "UpNextButton"];
 
-if (ShouldAttemptSkip) {
-    setInterval(() => {
-        for (const button of document.body.querySelectorAll("[role=\"button\"]")) {
-            if (SkipButtonTags.includes(button?.dataset["testid"]))
-                button.click();
-        }
-    }, SkipCheckSeconds * 1000);
-}
+setInterval(() => {
+    if (!ShouldAttemptSkip) return; 
+
+    for (const button of document.body.querySelectorAll("[role=\"button\"]")) {
+        if (SkipButtonTags.includes(button?.dataset["testid"]))
+            button.click();
+    }
+}, SkipCheckSeconds * 1000);
